32d1c3c7729dead0e52924e1f7a61a889211cec5
roy <roy@god.com>
2024-05-24 05:49:57 +0000

    . sass

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,23 @@
 import Phaser from 'phaser'
 
-import HelloWorldScene from './HelloWorldScene'
+import ReaderScene from "./ReaderScene";
 
 const config: Phaser.Types.Core.GameConfig = {
-	type: Phaser.AUTO,
-	parent: 'app',
-	width: 800,
-	height: 600,
-	physics: {
-		default: 'arcade',
-		arcade: {
-			gravity: { y: 200 },
-		},
-	},
-	scene: [HelloWorldScene],
+    type: Phaser.AUTO,
+    parent: 'app',
+    width: 800,
+    height: 600,
+    physics: {
+        default: 'arcade'
+    },
+    scene: [ReaderScene],
+    loader: {
+        crossOrigin: 'anonymous'          // 避免图片跨域
+    },
+    render: {
+        antialias: true,                  // 抗锯齿，建议开
+        transparent: true                 // Canvas容器背景透明，如果有和DOM有交互时这项可以开，记得把背景色关掉
+    },
 }
 
 export default new Phaser.Game(config)
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
+import {resolve} from "path";
 
 export default defineConfig({
+	resolve: {
+		alias: [
+			{
+				find: /^\/@\//,
+				replacement: `${resolve(__dirname, "./src")}/`,
+			},
+		],
+	},
 	plugins: [],
-	server: { host: '0.0.0.0', port: 8000 },
+	server: { host: '0.0.0.0', port: 8700 },
 	clearScreen: false,
+	//全局引入
+	css: {
+		preprocessorOptions: {
+			scss: {
+				additionalData: '@import "/@/style/common.scss";',
+			}
+		}
+	},
 })
